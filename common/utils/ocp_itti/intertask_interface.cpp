b7d5811d132b958575642afeec712f6358b888df
NicolePerrotta <perrottanicole00@gmail.com>
2024-06-25 15:08:07 -0400

    sposta include

--- common/utils/ocp_itti/intertask_interface.cpp
+++ common/utils/ocp_itti/intertask_interface.cpp
@@ -26,13 +26,13 @@
 #include <sys/eventfd.h>
 #include <semaphore.h>
 
-//PRB 
-#include "openair2/LAYER2/NR_MAC_gNB/myArray.h"
-
 extern "C" {
 #include <intertask_interface.h>
 #include <common/utils/system.h>
 #include "executables/softmodem-common.h"
+
+//PRB 
+#include "openair2/LAYER2/NR_MAC_gNB/myArray.h"
 
 typedef struct timer_elm_s {
   timer_type_t type; ///< Timer type
