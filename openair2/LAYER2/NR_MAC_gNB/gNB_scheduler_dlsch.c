60c4463e0f1c36f99faf53a8873f4e940da3fcc1
freebacca <villa.d@northeastern.edu>
2024-11-25 22:54:22 +0100

    max e non mcs

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_dlsch.c
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_dlsch.c
@@ -913,7 +913,6 @@ void slice_prb_estimate(module_id_t module_id,
       else mcs = bler_stats->mcs;
 
       //printf("estimated mcs = %u \n",mcs);
-      mcs = modified_mcs;   
 
       uint8_t nrOfLayers = get_dl_nrOfLayers(sched_ctrl, dl_bwp->dci_format);
       uint8_t Qm = nr_get_Qm_dl(mcs, dl_bwp->mcsTableIdx);
@@ -1033,7 +1032,7 @@ static void pf_dl_slice(module_id_t module_id,
     /* Calculate coeff */
     const NR_bler_options_t *bo = &mac->dl_bler;
     const int max_mcs_table = current_BWP->mcsTableIdx == 1 ? 27 : 28;
-    const int max_mcs = min(sched_ctrl->dl_max_mcs, max_mcs_table);
+    const int max_mcs = modified_mcs; //min(sched_ctrl->dl_max_mcs, max_mcs_table); NEW
     if (bo->harq_round_max == 1)
       sched_pdsch->mcs = max_mcs;
     else
