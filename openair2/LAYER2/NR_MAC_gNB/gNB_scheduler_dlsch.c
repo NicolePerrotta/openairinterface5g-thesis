7e4785857c9d98cd099924d5fe66492b24dca98b
freebacca <villa.d@northeastern.edu>
2024-11-26 22:37:12 +0100

    check formato json

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_dlsch.c
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_dlsch.c
@@ -657,7 +657,7 @@ void nr_update_slice_policy(module_id_t module_id, frame_t frame, sub_frame_t sl
     max_ratio = json_object_get_int(json_object_object_get(s_array_obj, "max_ratio"));
     dedicated_ratio = json_object_get_int(json_object_object_get(s_array_obj, "dedicated_ratio"));
     set_mcs = json_object_get_int(json_object_object_get(s_array_obj, "set_mcs"));
-
+    //set_mcs = atoi(set_mcs);
     modified_mcs = set_mcs; //NEW
 
     if (sd == NULL) {
@@ -1036,7 +1036,7 @@ static void pf_dl_slice(module_id_t module_id,
     if (bo->harq_round_max == 1)
       sched_pdsch->mcs = max_mcs;
     else
-      sched_pdsch->mcs = 26; //modified_mcs; //get_mcs_from_bler(bo, stats, &sched_ctrl->dl_bler_stats, max_mcs, frame); //NEW
+      sched_pdsch->mcs = modified_mcs; //get_mcs_from_bler(bo, stats, &sched_ctrl->dl_bler_stats, max_mcs, frame); //NEW
     sched_pdsch->nrOfLayers = get_dl_nrOfLayers(sched_ctrl, current_BWP->dci_format);
     sched_pdsch->pm_index = get_pm_index(mac, UE, current_BWP->dci_format, sched_pdsch->nrOfLayers, mac->radio_config.pdsch_AntennaPorts.XP);
     const uint8_t Qm = nr_get_Qm_dl(sched_pdsch->mcs, current_BWP->mcsTableIdx);
